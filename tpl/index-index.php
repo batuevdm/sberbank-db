d67e3c903a339f9b9ce473931daa7d63a98e2c48
Dmitry Batuev <37241354+batuevdm@users.noreply.github.com>
2018-05-25 10:12:03 +0400

    Update

--- tpl/index-index.php
+++ tpl/index-index.php
@@ -8,9 +8,9 @@
             <a href="/search/phone" class="btn btn-success">Найти по номеру телефона</a>
             <a href="/search/client_number" class="btn btn-success">Найти по номеру счета</a>
         </div>
-        <div class="row" style="margin-top: 20px;">
+        <!--<div class="row" style="margin-top: 20px;">
             <a href="/view/all" class="btn btn-primary">Просмотр всех вкладов</a>
-        </div>
+        </div>-->
         <div class="row" style="margin-top: 20px;">
             <a href="/deposit/new" class="btn btn-lg btn-danger">Открыть новый вклад</a>
         </div>
